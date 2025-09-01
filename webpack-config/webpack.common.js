52675f6cf50d0e403ed89c030b875576c4c057df
kenneth kang <pretty9967@naver.com>
2025-09-01 16:29:24 +0900

    added env file and webpack build

---
+++ webpack-config/webpack.common.js
@@ -0,0 +1,43 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = {
+    entry: '../src/index.ts',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'auto',
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.tsx', '.ts', '.css'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'designSystem',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './FeButton': '../src/components/button/FeButton.tsx',
+            },
+            shared: {
+                react: { singleton: true, requiredVersion: '18.2.0', eager: true },
+                'react-dom': { singleton: true, requiredVersion: '18.2.0', eager: true },
+            },
+        }),
+    ],
+};
---
+++ webpack-config/webpack.config.js
@@ -0,0 +1,64 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+    return {
+        entry: {
+            // npm 패키지용 진입점 (별도로 번들링)
+            lib: '../src/index.ts',
+        },
+        mode: argv.mode,
+        output: {
+            filename: () => {
+                // main과 lib 번들 파일명 분리
+                return 'lib.js';
+            },
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: 'auto',
+            libraryTarget: 'umd', // npm 패키지용 포맷
+        },
+        resolve: {
+            extensions: ['.jsx', '.js', '.tsx', '.ts', '.css'],
+        },
+        devServer: isDevelopment
+            ? {
+                  static: path.join(__dirname, 'dist'),
+                  port: 3001,
+              }
+            : undefined,
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader', 'postcss-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new Dotenv({
+                path: isDevelopment ? '../.env.development' : '../.env.production',
+            }),
+            new ModuleFederationPlugin({
+                name: 'designSystem',
+                filename: 'remoteEntry.js',
+                exposes: {
+                    './FeButton': '../src/components/button/FeButton.tsx',
+                },
+                shared: {
+                    react: { singleton: true, requiredVersion: '18.2.0', eager: true },
+                    'react-dom': { singleton: true, requiredVersion: '18.2.0', eager: true },
+                },
+            }),
+        ],
+    };
+};
